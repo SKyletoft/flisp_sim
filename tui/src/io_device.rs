c03aa516a0d2b4f38b7cef00908a14996f4038c4
Samuel Kyletoft <skyletoft@gmail.com>
2021-05-16 02:47:13 +0200

    IoDevice values + read

--- tui/src/io_device.rs
+++ tui/src/io_device.rs
@@ -1,11 +1,31 @@
-#[derive(Debug, Copy, Clone, PartialEq, variantly::Variantly)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub(crate) enum IoDevice {
 	Nothing,
-	Bargraph,
-	HexDisplay,
-	SevenSeg,
-	StepperMotor,
-	DILSwitch,
-	KeyPad,
-	IRQFlipFlop,
+	Bargraph(u8),
+	HexDisplay(u8),
+	SevenSeg(u8),
+	StepperMotor(u8),
+	DILSwitch(u8),
+	KeyPad(u8),
+	IRQFlipFlop(u8),
+}
+
+impl IoDevice {
+	pub(crate) fn read(&self) -> Option<u8> {
+		let res = match self {
+			IoDevice::Nothing => return None,
+			IoDevice::Bargraph(_) => 0,
+			IoDevice::HexDisplay(stored) => *stored,
+			IoDevice::SevenSeg(stored) => *stored,
+			IoDevice::StepperMotor(stored) => *stored,
+			IoDevice::DILSwitch(stored) => *stored,
+			IoDevice::KeyPad(stored) => *stored,
+			IoDevice::IRQFlipFlop(stored) => *stored,
+		};
+		Some(res)
+	}
+
+	pub(crate) fn to_widget<W: tui::widgets::Widget>(&self) -> Box<W> {
+		todo!()
+	}
 }
